d75e54dd1c78dc35fc2449888490bc457d432ce2
hosik-2 <zxvbcd123@gmail.com>
2026-04-27 19:59:51 +0900

    인터셉터 등록까지 완료

--- exception/src/main/java/hello/exception/WebConfig.java
+++ exception/src/main/java/hello/exception/WebConfig.java
@@ -1,17 +1,30 @@
 package hello.exception;
 
 import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    //스프링이 mvc 초기화 후 WebConfig 한테 인터셉터 명단 요청이 생기면
+    // addInterceptor() 실행 후 registry 안에 저장
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "*.ico", "/error", "/error-page/**");// 오류 페이지 경로 제외
+    }
+
+    @Bean //스프링이 실행 후 빈 등록 메서드 동작 다음 반환 객체를 빈 보관함에 보관 후
+    //mvc 초기화 후 인터셉터 등록 후 스프링이 톰캣으로 필터 전달
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
