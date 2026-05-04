a4cd37a17c3df6765ef60c3d1a164f154f527ed2
hosik-2 <zxvbcd123@gmail.com>
2026-05-04 22:46:33 +0900

    argumentResolver 적용 완료(숙지 미흡)

--- exception/src/main/java/hello/exception/WebServerCustomizer.java
+++ exception/src/main/java/hello/exception/WebServerCustomizer.java
@@ -6,7 +6,7 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component // BasicErrorController 사용을 위해 주석 처리
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
 
     @Override
